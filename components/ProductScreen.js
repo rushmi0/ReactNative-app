56c9829ee808c1b51d579231a31c9ed58b46640b
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-08-26 11:48:56 +0700

    เย้.... รูปมาแล้ววว

---
+++ components/ProductScreen.js
@@ -0,0 +1,34 @@
+import {
+    View,
+    Text,
+    FlatList
+} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+const ProductScreen = () => {
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get('https://api.codingthailand.com/api/course')
+            setProduct(response.data.data)
+        }
+        getData();
+    }, []);
+
+
+    return (
+        <View>
+            <FlatList
+                data={product}
+                keyExtractor={(item,index) => item.id.toString()}
+                renderItem={({item}) => (
+                    <Text>{item.title}</Text>
+                )}
+            />
+        </View>
+    )
+}
+export default ProductScreen
