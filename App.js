1d318502731b5e3aeef79684735c686ecea86d7d
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-09-02 09:21:27 +0700

    เช็คชื่อ

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import TouchablePractice from './components/TouchablePractice'
-import Example_useEffect from './components/Example_useEffect'
-import UseEffectFlatList from './components/UseEffectFlatList'
+import {Text, View} from 'react-native'
+import React, {Component} from 'react'
 
-import RandomUsersScreen from './components/RandomUsersScreen'
-import FlatList_Example1 from './components/FlatList_Example1'
-import FlatListHeaderFooter from "./components/FlatList_HeaderFooter";
-import FlastListApi from "./components/FlastListAPI";
-import News from "./components/News";
-import ProductScreen from "./components/ProductScreen";
-
-const App = () => {
-  return (
-    <View style={ {flex: 1, alignItems: "left"} }>
-      {/* <TouchablePractice/> */}
-      {/* <Example_useEffect/> */}
-      {/* <UseEffectFlatList/> */}
-
-      {/* <RandomUsersScreen/> */}
-      {/*<FlatList_Example1/>*/}
-
-      {/*<FlatListHeaderFooter/>*/}
-      {/*<FlastListApi/>*/}
-      {/* <News/>*/}
-
-        <ProductScreen/>
-    </View>
-  )
+export class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>App</Text>
+            </View>
+        )
+    }
 }
 
 export default App
-
--- README.md
+++ README.md
@@ -3,17 +3,12 @@
 <br>
 ชื่อ: วัชรพล พงษ์วิลัย
 <br>
-วันที่: 19/08/2023
+วันที่: 02/09/2023
 <br>
 ```txt
-1. PR Mobile Application Contest as a link: 
-https://www.facebook.com/100094507495416/posts/pfbid0354LSgwXyzZqV5pR6DiifYJLJaL5vcLcyB2HD57cs7kfCAcH8DVXzRb8ezLtvFPRXl/?mibextid=cr9u03
-
-2. using Android Emulator
-
-3. using Fetch call API in Component
-
-4. Guideline: Postman calls the API
-
-5. In-class exercise
+    Lecture #9 [September 2, 2023]: Class Agenda
+    
+    1. Take a questionnaire (from Aj.Lalita) as a link: https://forms.gle/ojZU31s3aUYmBDZp8
+    2. React Navigator : Stack Navigator
+    3. In-class exercise
 ```
