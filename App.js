5f1388dc86d1aa03a9bbbe537a5e5ab3b6479674
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-09-02 10:16:35 +0700

    Navigator เปลี่ยนหน้า

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
-import {Text, View} from 'react-native'
+import {Text, View, Button} from 'react-native'
 import React, {Component} from 'react'
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import DetailsScreen from "./screens/DetailsScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+// const HomeScreen = ({navigation}) => {
+//     return (
+//         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//             <Text>Home Screen</Text>
+//             <Button
+//                 title='Go to Details'
+//                 onPress={ () => navigation.navigate('Details')}
+//             />
+//         </View>
+//     )
+// }
+//
+// const DetailScreen = () => {
+//     return (
+//         <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+//             <Text>Detail Screen</Text>
+//         </View>
+//     )
+// }
 
 export class App extends Component {
     render() {
         return (
-            <View>
-                <Text>App</Text>
-            </View>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName='Home'>
+
+                    <Stack.Screen
+                        name='Home'
+                        component={HomeScreen}
+                        options={{title: 'Overview'}}
+                    />
+
+                    <Stack.Screen
+                        name='Details'
+                        component={DetailsScreen}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
         )
     }
 }
+
+
+const Stack = createNativeStackNavigator()
 
 export default App
---
+++ screens/DetailsScreen.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const DetailsScreen = () => {
+    return (
+        <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Detail Screen</Text>
+        </View>
+    )
+}
+export default DetailsScreen
